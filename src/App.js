05a5f8c98ff91f5def466fbada1b9be4c6f0f697
Polas Chandra Barmon <dpi.polas@gmail.com>
2024-07-12 01:48:09 +0600

    added Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import Header from './Components/Header/Header';
-import Shop from './Components/Shop/Shop';
+import * as React from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./Components/NotFounf/NotFound";
+import Review from "./Components/Review/Review";
+import Inventory from "./Components/Inventory/Inventory";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Shop />} />
+          <Route index element={<Shop />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="review" element={<Review />} />
+          <Route path="manage" element={<Inventory />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Review = () => {
+  return (
+    <div>
+      <h4>Review Under Development</h4>
+    </div>
+  );
+};
+
+export default Review;
